0f0bb173f611046dc41512b218c1ad8af8f61db0
liqingwei <lqw332664203@mail.ustc.edu.cn>
2020-10-03 14:08:14 +0800

    slice_test.py

--- python_pro/study/高级特性/slice_test.py
+++ python_pro/study/高级特性/slice_test.py
@@ -1,4 +1,36 @@
-def trim(s)
-{
-    
-}
+import sys
+def trim(s):
+    s1, s2 = '', ''
+    flag = 0
+    for i in s:
+        if flag:
+            s1 = s1+i
+        elif (i != ' ') & (flag == 0):
+            s1 = s1+i
+            flag = 1
+    s1 = s1[::-1]
+    flag = 0
+    for i in s1:
+        if flag:
+            s2 = s2+i
+        elif (i != ' ') & (flag == 0):
+            s2 = s2+i
+            flag = 1
+    s2 = s2[::-1]
+    return s2
+
+# 测试:
+if trim('hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello') != 'hello':
+    print('测试失败!')
+elif trim('  hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello  world  ') != 'hello  world':
+    print('测试失败!')
+elif trim('') != '':
+    print('测试失败!')
+elif trim('    ') != '':
+    print('测试失败!')
+else:
+    print('测试成功!')
