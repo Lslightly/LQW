4cdfac6427123e300b543dec0a0092a433bca659
liqingwei <lqw332664203@mail.ustc.edu.cn>
2021-01-13 10:48:21 +0800

    哈弗曼树代码简化版加实验报告

--- C++/DataStructure/HuffmanTree/HuffmanTree.h
+++ C++/DataStructure/HuffmanTree/HuffmanTree.h
@@ -6,17 +6,21 @@
 #define MAX_CHARS 1000
 typedef struct HTNode
 {
-    unsigned int i;
-    unsigned int weight;
-    unsigned int parent, lchild, rchild;
+    unsigned int i;     //  i为在HT中的编号
+    unsigned int weight;//  权重
+    unsigned int parent, lchild, rchild;    //  左右孩子双亲指针
 }HTNode, * HuffmanTree;
 
 typedef char ** HuffmanCode;
 
+//  在下标<=i的范围内选择最小的两个没有parent的节点，通过s1,s2返回下标
 void Select(HuffmanTree & HT, int i, int & s1, int & s2);
 
+//  哈夫曼编码
+//  n为叶子节点个数
 void HuffmanDecoding(HuffmanTree & HT, HuffmanCode &HC, int &n);
 
+//  权值比较函数
 int cmp_weight(const HTNode & a, const HTNode & b);
 
 //  HC: Huffman Code
@@ -31,26 +35,27 @@ void HuffmanCoding(HuffmanTree & HT, HuffmanCode & HC, unsigned int * w, int n)
     
     Priority_Queue<HTNode> HT_queue;
 
+    //  构造哈夫曼树
     HT_queue.init(HT_queue, m, cmp_weight);
-    for (p = HT+1, i = 1; i <= n; i++, ++w, ++p)
+    for (p = HT+1, i = 1; i <= n; i++, ++w, ++p)    //  先将叶子节点全部入队
     {
         *p = {(unsigned int)i, *w, 0, 0, 0};
         HT_queue.enqueue(HT_queue, *p);
     }
     
-    for (i = n+1; i <= m; i++)
+    for (i = n+1; i <= m; i++)  //  通过这么多此循环，将哈弗曼树的所有节点都构造出来
     {
         HTNode min1, min2;
         HT_queue.dequeue(HT_queue, min1);
         HT_queue.dequeue(HT_queue, min2);
-        // unsigned int mini = (min1.i < min2.i)?min1.i:min2.i;
-        // unsigned int maxi = (mini == min1.i)?min2.i:min1.i;
-        HT[i] = {(unsigned int)i, min1.weight+min2.weight, 0, min1.i, min2.i};
+        HT[i] = {(unsigned int)i, min1.weight+min2.weight, 0, min1.i, min2.i};  //  构造parent节点
         HT[min1.i].parent = (unsigned int)i;
         HT[min2.i].parent = (unsigned int)i;
         HT_queue.enqueue(HT_queue, HT[i]);
     }
 
+    //  构造哈夫曼编码
+    //  从下往上构造
     HC = (HuffmanCode)malloc(sizeof(*HC)*(n+1));
     char * cd = (char *)malloc(n * sizeof(char));
 
@@ -61,7 +66,7 @@ void HuffmanCoding(HuffmanTree & HT, HuffmanCode & HC, unsigned int * w, int n)
 
         for (; f != 0; c = f, f = HT[c].parent)
         {
-            if (HT[f].lchild == c) cd[--start] = '0';
+            if (HT[f].lchild == c) cd[--start] = '0';   //  判断左右孩子来决定是加0还是加1
             else cd[--start] = '1';
         }
 
